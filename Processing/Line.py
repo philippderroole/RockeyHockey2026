36b6eb1f9d0d2ef64fca902afd7b031b82e9dcbc
lukaskarg <lukas.karg@googlemail.com>
2023-05-11 16:09:25 +0200

    Hilfsklasse Line

--- Processing/Line.py
+++ Processing/Line.py
@@ -2,11 +2,14 @@ import math
 
 
 class Line:
-    def __init__(self, p1, p2):
+    def __init__(self, p1, p2=None, m=None):
         self.p1 = p1
-        self.p2 = p2
-        self.m = (p2[1] - p1[1]) / (p2[0] - p1[0])
-        self.b = p1[1] - self.m * p1[0]
+        if p2 is not None:
+            self.m = (p2[1] - p1[1]) / (p2[0] - p1[0])
+            self.b = p1[1] - self.m * p1[0]
+        else:
+            self.m = m
+            self.b = p1[1] - self.m * p1[0]
 
     def get_y(self, x):
         return self.m * x + self.b
