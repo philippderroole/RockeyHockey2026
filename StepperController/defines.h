c68c7a8e9d6a6a18b98113c9d44d59ebc3894d4e
Lukas Karg <lukas.karg@googlemail.com>
2023-06-16 10:31:11 +0200

    changed max values

--- StepperController/defines.h
+++ StepperController/defines.h
@@ -8,8 +8,8 @@
 #define MAX_ACCEL 10000
 #define MAX_SPEED 10000
 #define MIN_SPEED 0
-#define MAX_X 1940
-#define MAX_Y 2900
+#define MAX_X 2200
+#define MAX_Y 1700
 #define END_PIN_Y 9
 #define END_PIN_X 10
 #endif
